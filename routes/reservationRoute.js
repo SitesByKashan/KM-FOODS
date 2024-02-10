cee3a157e6ed11be774d3549b5b2f05c7787a6cc
Kashan Malik Awan <sitesbykashan@gmail.com>
2024-02-10 19:10:55 +0500

    changes

--- routes/reservationRoute.js
+++ routes/reservationRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { sendReservation } from "../Controller/reservation";
+import { sendReservation } from "../Controller/reservation.js";
 const router = express.Router();
 
 router.post("/send", sendReservation);
